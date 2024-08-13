c0221ac5b90bfc694010d87cfd64560b6516b96d
root <root@DESKTOP-A0HNFIU.>
2024-08-13 23:27:25 +0300

    task done

---
+++ soapcalculator/src/main/java/com/exalt/training/soapcalculator/CalculatorService.java
@@ -0,0 +1,2 @@
+package com.exalt.training.soapcalculator;public interface CalculatorService {
+}
--- soapcalculator/src/main/java/com/exalt/training/soapcalculator/SoapCalculatorApplication.java
+++ soapcalculator/src/main/java/com/exalt/training/soapcalculator/SoapCalculatorApplication.java
@@ -1,13 +1,2 @@
-package com.exalt.training.soapcalculator;
-
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
-
-@SpringBootApplication
-public class SoapcalculatorApplication {
-
-	public static void main(String[] args) {
-		SpringApplication.run(SoapcalculatorApplication.class, args);
-	}
-
+package com.exalt.training.soapcalculator;public class sdfsdf {
 }
