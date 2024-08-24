1e81395eab1020b828db8644eeb37af4bc70b72a
root <root@DESKTOP-A0HNFIU.>
2024-08-24 22:36:57 +0300

    micro-services

---
+++ micro-services/school/src/main/java/com/exalt/training/school/School.java
@@ -0,0 +1,20 @@
+package com.exalt.training.school;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import lombok.*;
+
+@Entity
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class School {
+    @Id
+    @GeneratedValue
+    private Integer id;
+    private String name;
+    private String email;
+}
---
+++ micro-services/student/src/main/java/com/exalt/training/student/StudentController.java
@@ -0,0 +1,33 @@
+package com.exalt.training.student;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/v1/students")
+@RequiredArgsConstructor
+public class StudentController {
+    private final StudentService studentService;
+
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    public void saveStudent(@RequestBody Student student) {
+        studentService.saveStudent(student);
+    }
+
+    @GetMapping
+    public ResponseEntity<List<Student>> findAllStudents() {
+        return ResponseEntity.ok(studentService.findAllStudents());
+    }
+
+    @GetMapping("/school/{school-id}")
+    public ResponseEntity<List<Student>> findAllStudents(
+            @PathVariable("school-id") Integer schoolId
+    ) {
+        return ResponseEntity.ok(studentService.findAllStudentsBySchool(schoolId));
+    }
+}
