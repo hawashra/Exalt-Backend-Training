d89319b718fcf21544a71175ad518452be69c338
hawashra <hamzaawashra100@gmail.com>
2024-09-02 14:25:02 +0300

    kafka message broker producer consumer java example

---
+++ kafka_producer_consumer/src/main/java/com/exalt/training/kafka_producer_consumer/config/KafkaTopicConfig.java
@@ -0,0 +1,2 @@
+package com.exalt.training.kafka_producer_consumer.config;public class KafkaTopicConfig {
+}
---
+++ kafka_producer_consumer/src/main/java/com/exalt/training/kafka_producer_consumer/consumer/KafkaConsumer.java
@@ -0,0 +1,2 @@
+package com.exalt.training.kafka_producer_consumer.consumer;public class KafkaConsumer {
+}
