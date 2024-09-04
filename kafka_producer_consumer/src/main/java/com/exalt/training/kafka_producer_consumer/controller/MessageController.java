34b5e78f013e6daa75e854e9a4ee0e801c00acee
hawashra <hamzaawashra100@gmail.com>
2024-09-04 14:32:37 +0300

    kafka producer consumer

--- kafka_producer_consumer/src/main/java/com/exalt/training/kafka_producer_consumer/controller/MessageController.java
+++ kafka_producer_consumer/src/main/java/com/exalt/training/kafka_producer_consumer/controller/MessageController.java
@@ -1,2 +1,32 @@
-package com.exalt.training.kafka_producer_consumer.controller;public class MessageController {
+package com.exalt.training.kafka_producer_consumer.controller;
+
+import com.exalt.training.kafka_producer_consumer.producer.KafkaProducer;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+/**
+ * REST controller for handling message-related requests.
+ */
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/api/v1/messages")
+public class MessageController {
+
+    // KafkaProducer for sending messages to Kafka topics
+    private final KafkaProducer kafkaProducer;
+
+    /**
+     * Endpoint for sending a message to the Kafka topic.
+     *
+     * @param message the message to send
+     * @return a response entity indicating the result
+     */
+    @PostMapping
+    public ResponseEntity<String> sendMessage(
+            @RequestBody String message
+    ) {
+        kafkaProducer.sendMessage(message);
+        return ResponseEntity.ok("Message sent successfully!");
+    }
 }
